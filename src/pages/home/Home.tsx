524b5ec8a217586b40700d8c4a479c786d13394d
RenanC <renancostam@gmail.com>
2021-01-14 23:04:12 -0300

    Home without lady load components

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,18 +1,23 @@
 import { useContextBusiness } from 'app/state/business/context';
 import BankingInformation from 'pages/business-profile/BankingInformation';
+import BusinessProfile from 'pages/business-profile/BusinessProfile';
+import DeliveryArea from 'pages/delivery-area/DeliveryArea';
+import Menu from 'pages/menu/Menu';
+import Orders from 'pages/orders/OrdersPage';
 import PageLayout from 'pages/PageLayout';
 import React from 'react';
 import { Redirect, Route, Switch, useRouteMatch } from 'react-router-dom';
+import Dashboard from './Dashboard';
 
-const Dashboard = React.lazy(() => import(/* webpackPrefetch: true */ 'pages/home/Dashboard'));
-const Menu = React.lazy(() => import(/* webpackPrefetch: true */ 'pages/menu/Menu'));
-const Orders = React.lazy(() => import(/* webpackPrefetch: true */ 'pages/orders/OrdersPage'));
-const DeliveryArea = React.lazy(
-  () => import(/* webpackPrefetch: true */ 'pages/delivery-area/DeliveryArea')
-);
-const BusinessProfile = React.lazy(
-  () => import(/* webpackPrefetch: true */ 'pages/business-profile/BusinessProfile')
-);
+//const Dashboard = React.lazy(() => import(/* webpackPrefetch: true */ 'pages/home/Dashboard'));
+//const Menu = React.lazy(() => import(/* webpackPrefetch: true */ 'pages/menu/Menu'));
+//const Orders = React.lazy(() => import(/* webpackPrefetch: true */ 'pages/orders/OrdersPage'));
+//const DeliveryArea = React.lazy(
+//  () => import(/* webpackPrefetch: true */ 'pages/delivery-area/DeliveryArea')
+//);
+//const BusinessProfile = React.lazy(
+//  () => import(/* webpackPrefetch: true */ 'pages/business-profile/BusinessProfile')
+//);
 //const BankingInformation = React.lazy(
 //  () => import(/* webpackPrefetch: true */ 'pages/business-profile/BankingInformation')
 //);
