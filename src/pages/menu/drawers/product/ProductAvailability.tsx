fc8e13790db5a473318d47b9f6ef7c8a78f73df5
RenanC <renancostam@gmail.com>
2021-01-25 18:20:52 -0300

    Add radio group to ProductAvailability

--- src/pages/menu/drawers/product/ProductAvailability.tsx
+++ src/pages/menu/drawers/product/ProductAvailability.tsx
@@ -1,4 +1,4 @@
-import { Text } from '@chakra-ui/react';
+import { Flex, Radio, RadioGroup, Text } from '@chakra-ui/react';
 import { useProductContext } from 'pages/menu/context/ProductContext';
 import React from 'react';
 import { Redirect, useRouteMatch } from 'react-router-dom';
@@ -9,6 +9,7 @@ export const ProductAvailability = () => {
   const { url } = useRouteMatch();
   const { productId } = useProductContext();
   //state
+  const [mainAvailability, setMainAvailability] = React.useState<string>('1');
 
   if (productId === 'new') {
     const urlRedirect = url.split('/availability')[0];
@@ -23,6 +24,22 @@ export const ProductAvailability = () => {
       <Text fontSize="sm">
         {t('Defina quais os dias e horários seus clientes poderão comprar esse ítem')}
       </Text>
+      <RadioGroup
+        onChange={(value) => setMainAvailability(value.toString())}
+        value={mainAvailability}
+        defaultValue="1"
+        colorScheme="green"
+        color="black"
+      >
+        <Flex flexDir="column" justifyContent="flex-start">
+          <Radio mt="2" value="1">
+            {t('Sempre disponível quando o restaurante aberto')}
+          </Radio>
+          <Radio mt="2" value="2">
+            {t('Disponível em dias e horários específicos')}
+          </Radio>
+        </Flex>
+      </RadioGroup>
     </>
   );
 };
