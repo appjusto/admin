2d51dbec8543233e19f7d3653b643920c5bcb8a3
RenanC <renancostam@gmail.com>
2021-03-22 20:08:34 -0300

    Fix landing zIndex

--- src/pages/landing/Content.tsx
+++ src/pages/landing/Content.tsx
@@ -8,7 +8,7 @@ export const Content = (props: FlexProps) => {
       alignItems="flex-start"
       position="relative"
       maxW={{ md: '312px', lg: '480px', xl: '656px' }}
-      zIndex="999"
+      zIndex="100"
       {...props}
     >
       {props.children}
--- src/pages/landing/LandingPageFooter.tsx
+++ src/pages/landing/LandingPageFooter.tsx
@@ -161,7 +161,7 @@ export const LandingPageFooter = () => {
         bottom={{ md: '0' }}
         bg="black"
         py="2"
-        zIndex="900"
+        zIndex="990"
       >
         <Container
           pt="0"
