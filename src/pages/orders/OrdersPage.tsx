850db72ac209a43b902faadbfe10c3fca36dba06
RenanC <renancostam@gmail.com>
2021-04-06 18:00:09 -0300

    Fix OrdersPage mt for personification

--- src/pages/orders/OrdersPage.tsx
+++ src/pages/orders/OrdersPage.tsx
@@ -1,4 +1,5 @@
 import { Box } from '@chakra-ui/react';
+import { useFirebaseUserRole } from 'app/api/auth/useFirebaseUserRole';
 import { useContextBusiness } from 'app/state/business/context';
 import Container from 'common/components/Container';
 import React from 'react';
@@ -11,6 +12,7 @@ import { OrdersHeader } from './OrdersHeader';
 
 const OrdersPage = () => {
   // context
+  const { isStaff } = useFirebaseUserRole();
   const { business } = useContextBusiness();
   const { path } = useRouteMatch();
   const history = useHistory();
@@ -21,7 +23,7 @@ const OrdersPage = () => {
   }
   return (
     <OrdersContextProvider>
-      <Box>
+      <Box mt={isStaff ? '80px' : '0'}>
         <OrdersHeader />
         <Container maxW={{ base: '100%', md: '740px', lg: '1260px' }}>
           <OrdersKanban />
