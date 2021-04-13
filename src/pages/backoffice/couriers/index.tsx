816f1509e23217d400d82dbc4f5684857c2a1da7
RenanC <renancostam@gmail.com>
2021-04-13 15:20:07 -0300

    Start to edit backoffice couriers page

--- src/pages/backoffice/couriers/index.tsx
+++ src/pages/backoffice/couriers/index.tsx
@@ -2,7 +2,6 @@ import { DeleteIcon } from '@chakra-ui/icons';
 import { Checkbox, CheckboxGroup, Flex, HStack, Text } from '@chakra-ui/react';
 import { CourierProfile, WithId } from 'appjusto-types';
 import { FilterText } from 'common/components/backoffice/FilterText';
-import { CustomButton } from 'common/components/buttons/CustomButton';
 import { CustomInput } from 'common/components/form/input/CustomInput';
 import React from 'react';
 import { Route, Switch, useHistory, useRouteMatch } from 'react-router-dom';
@@ -21,7 +20,7 @@ const CouriersPage = () => {
   const couriers = [] as WithId<CourierProfile>[];
   // state
   const [dateTime, setDateTime] = React.useState('');
-  const [searchId, setSearchId] = React.useState('');
+  const [search, setSearch] = React.useState('');
   const [searchName, setSearchName] = React.useState('');
 
   const [filterText, setFilterText] = React.useState('all');
@@ -44,27 +43,15 @@ const CouriersPage = () => {
     <>
       <PageHeader title={t('Entregadores')} subtitle={t(`Atualizado ${dateTime}`)} />
       <Flex mt="8" justifyContent="space-between">
-        <HStack spacing={4}>
-          <CustomInput
-            mt="0"
-            maxW="212px"
-            id="search-id"
-            value={searchId}
-            onChange={(event) => setSearchId(event.target.value)}
-            label={t('ID')}
-            placeholder={t('000')}
-          />
-          <CustomInput
-            mt="0"
-            w="280px"
-            id="search-name"
-            value={searchName}
-            onChange={(event) => setSearchName(event.target.value)}
-            label={t('Nome')}
-            placeholder={t('Nome do restaurante')}
-          />
-        </HStack>
-        <CustomButton mt="0" maxW="200px" label={t('Filtrar resultados')} />
+        <CustomInput
+          mt="0"
+          w="400px"
+          id="search-id"
+          value={search}
+          onChange={(event) => setSearch(event.target.value)}
+          label={t('Buscar')}
+          placeholder={t('Buscar por ID ou nome')}
+        />
       </Flex>
       <Flex mt="8" w="100%" justifyContent="space-between" borderBottom="1px solid #C8D7CB">
         <HStack spacing={4}>
