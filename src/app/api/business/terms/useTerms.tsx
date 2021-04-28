f7469abb3e9e31c2472f214e62ed7fac54ca57c3
RenanC <renancostam@gmail.com>
2021-04-28 12:47:55 -0300

    First steps to TermsOfUse page

---
+++ src/app/api/business/terms/useTerms.tsx
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import marked from 'marked';
+import React from 'react';
+import { useQuery } from 'react-query';
+
+export const useTerms = () => {
+  // context
+  const url =
+    'https://github.com/appjusto/docs/blob/532a6b057ab8a891db8018b4f4588a66a21ae339/legal/termos-de-uso-restaurantes.md';
+  // state
+  const [unformattedTerms, setUnformattedTerms] = React.useState<string | null>();
+  const [formattedTerms, setFormattedTerms] = React.useState<string | null>();
+
+  // side effects
+  const query = useQuery(['terms'], () => axios.get<string>(url));
+
+  React.useEffect(() => {
+    if (query.data?.data) setUnformattedTerms(query.data.data);
+    else if (query.isError) setUnformattedTerms(null);
+  }, [query]);
+
+  React.useEffect(() => {
+    if (unformattedTerms) setFormattedTerms(marked(unformattedTerms));
+  }, [unformattedTerms]);
+  // result
+  return formattedTerms;
+};
