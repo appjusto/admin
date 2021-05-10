74ede6b92a707ec83b90f4bf0f649029f7011b65
RenanC <renancostam@gmail.com>
2021-05-10 12:04:13 -0300

    BO order drawer for p2p and food orders

--- src/pages/backoffice/drawers/order/Details.tsx
+++ src/pages/backoffice/drawers/order/Details.tsx
@@ -4,70 +4,81 @@ import React from 'react';
 import { itemPriceFormatter } from 'utils/formatters';
 import { getOrderTotalPriceToDisplay } from 'utils/functions';
 import { t } from 'utils/i18n';
+import { SectionTitle } from '../generics/SectionTitle';
 
 interface DetailsProps {
   order?: WithId<Order> | null;
 }
 
 export const Details = ({ order }: DetailsProps) => {
-  const orderTotalPrice = getOrderTotalPriceToDisplay(order?.items || []);
+  // state
+  const [totalPrice, setTotalPrice] = React.useState<string>();
+
+  // side effects
+  React.useEffect(() => {
+    if (order?.type === 'food') setTotalPrice(getOrderTotalPriceToDisplay(order?.items || []));
+    else setTotalPrice(itemPriceFormatter(order?.fare?.consumer.total ?? 0));
+  }, [order?.type, order?.items]);
+
+  // UI
   return (
     <Box>
-      <Text mt="6" fontSize="xl" color="black">
-        {t('Detalhes do pedido')}
-      </Text>
-      <Table size="md" variant="simple">
-        <Thead>
-          <Tr>
-            <Th>{t('Item')}</Th>
-            <Th isNumeric>{t('Qtde.')}</Th>
-            <Th isNumeric>{t('Valor/Item')}</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {order?.items?.map((item: OrderItem) => (
-            <React.Fragment key={Math.random()}>
-              <Tr color="black" fontSize="xs" fontWeight="700">
-                <Td>{item.product.name}</Td>
-                <Td isNumeric>{item.quantity}</Td>
-                <Td isNumeric>{itemPriceFormatter(item.product.price * item.quantity)}</Td>
+      {order?.type === 'food' && (
+        <>
+          <SectionTitle mt="10">{t('Detalhes do pedido')}</SectionTitle>
+          <Table size="md" variant="simple">
+            <Thead>
+              <Tr>
+                <Th>{t('Item')}</Th>
+                <Th isNumeric>{t('Qtde.')}</Th>
+                <Th isNumeric>{t('Valor/Item')}</Th>
               </Tr>
-              {item.complements &&
-                item.complements.map((complement) => (
-                  <Tr key={Math.random()} fontSize="xs">
-                    <Td>{complement.name}</Td>
-                    <Td isNumeric>1</Td>
-                    <Td isNumeric>{itemPriceFormatter(complement.price)}</Td>
+            </Thead>
+            <Tbody>
+              {order?.items?.map((item: OrderItem) => (
+                <React.Fragment key={Math.random()}>
+                  <Tr color="black" fontSize="xs" fontWeight="700">
+                    <Td>{item.product.name}</Td>
+                    <Td isNumeric>{item.quantity}</Td>
+                    <Td isNumeric>{itemPriceFormatter(item.product.price * item.quantity)}</Td>
                   </Tr>
-                ))}
-            </React.Fragment>
-          ))}
-        </Tbody>
-        <Tfoot bgColor="gray.50">
-          <Tr color="black">
-            <Th>{t('Valor total de itens:')}</Th>
-            <Th></Th>
-            <Th isNumeric>{orderTotalPrice}</Th>
-          </Tr>
-        </Tfoot>
-      </Table>
-      <Text mt="10" fontSize="xl" color="black">
-        {t('Observações')}
-      </Text>
+                  {item.complements &&
+                    item.complements.map((complement) => (
+                      <Tr key={Math.random()} fontSize="xs">
+                        <Td>{complement.name}</Td>
+                        <Td isNumeric>1</Td>
+                        <Td isNumeric>{itemPriceFormatter(complement.price)}</Td>
+                      </Tr>
+                    ))}
+                </React.Fragment>
+              ))}
+            </Tbody>
+            <Tfoot bgColor="gray.50">
+              <Tr color="black">
+                <Th>{t('Valor total de itens:')}</Th>
+                <Th></Th>
+                <Th isNumeric>{totalPrice}</Th>
+              </Tr>
+            </Tfoot>
+          </Table>
+        </>
+      )}
+      <SectionTitle mt="10">{t('Observações')}</SectionTitle>
       <Text mt="1" fontSize="md">
         {t('Incluir CPF na nota, CPF: 000.000.000-00')}
       </Text>
-      <Text mt="10" fontSize="xl" color="black">
-        {t('Forma de pagamento')}
-      </Text>
+      <SectionTitle mt="10">{t('Forma de pagamento')}</SectionTitle>
       <Text mt="1" fontSize="md">
         {t('Total pago:')}{' '}
         <Text as="span" color="black">
-          {orderTotalPrice}
+          {totalPrice}
         </Text>
       </Text>
       <Text mt="1" fontSize="md">
-        {t('Método de pagamento:')} <Text as="span" color="black"></Text>
+        {t('Método de pagamento:')}{' '}
+        <Text as="span" color="black">
+          N/I
+        </Text>
       </Text>
     </Box>
   );
