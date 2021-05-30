df381683426c313e22516f4c46e332b8033d7e65
RenanC <renancostam@gmail.com>
2021-05-30 13:35:25 -0300

    Update Chakra and framer-motion and create ChatButton

---
+++ src/pages/orders/kanban/ChatButton.tsx
@@ -0,0 +1,50 @@
+import { Button, ButtonProps, Text } from '@chakra-ui/react';
+import { ReactComponent as ChatIcon } from 'common/img/chat.svg';
+import { motion, useAnimation } from 'framer-motion';
+import React from 'react';
+import { t } from 'utils/i18n';
+
+const MotionButton = motion<ButtonProps>(Button);
+
+interface ChatButtonProps {
+  isNewMessage: boolean;
+}
+
+export const ChatButton = ({ isNewMessage }: ChatButtonProps) => {
+  const controls = useAnimation();
+
+  React.useEffect(() => {
+    if (isNewMessage) {
+      controls.start({
+        scale: [1, 1.06, 1.06, 1, 1],
+        transition: {
+          duration: 1,
+          ease: 'easeInOut',
+          //times: [0, 0.2, 0.5],
+          times: [0, 0.2, 0.2, 0.8, 1],
+          repeat: Infinity,
+          repeatType: 'loop',
+          repeatDelay: 0.8,
+        },
+      });
+    } else {
+      controls.stop();
+    }
+  }, [isNewMessage]);
+
+  return (
+    <MotionButton
+      bg={isNewMessage ? '#FFBE00' : 'white'}
+      variant="outline"
+      minW="100px"
+      height="60px"
+      borderColor="black"
+      fontWeight="700"
+      color="black"
+      animate={controls}
+    >
+      <ChatIcon />
+      <Text ml="4">{t('Chat')}</Text>
+    </MotionButton>
+  );
+};
--- src/pages/orders/kanban/OrdersKanban.tsx
+++ src/pages/orders/kanban/OrdersKanban.tsx
@@ -1,6 +1,5 @@
 import {
   Box,
-  Button,
   Flex,
   HStack,
   Icon,
@@ -12,13 +11,13 @@ import {
 } from '@chakra-ui/react';
 import { splitByStatus } from 'app/api/order/selectors';
 import { Order, OrderStatus, WithId } from 'appjusto-types';
-import { ReactComponent as ChatIcon } from 'common/img/chat.svg';
 import { ReactComponent as SearchIcon } from 'common/img/searchIcon.svg';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { getDateTime } from 'utils/functions';
 import { t } from 'utils/i18n';
 import { useOrdersContext } from '../context';
+import { ChatButton } from './ChatButton';
 import { OrderSearchResult } from './OrderSearchResult';
 import { OrdersKanbanList } from './OrdersKanbanList';
 
@@ -48,6 +47,7 @@ export const OrdersKanban = () => {
       setSearchResult(result);
     }
   }, [orders, orderSearch]);
+
   // UI
   return (
     <Box pb="12">
@@ -115,18 +115,7 @@ export const OrdersKanban = () => {
               />
             </InputGroup>
             <Link to="/app/chat">
-              <Button
-                bg={isNewChatMessage ? '#FFBE00' : 'white'}
-                variant="outline"
-                minW="100px"
-                height="60px"
-                borderColor="black"
-                fontWeight="700"
-                color="black"
-              >
-                <ChatIcon />
-                <Text ml="4">{t('Chat')}</Text>
-              </Button>
+              <ChatButton isNewMessage={isNewChatMessage} />
             </Link>
           </HStack>
           <Text mt="4" fontSize="sm" color="grey.700">
@@ -146,13 +135,6 @@ export const OrdersKanban = () => {
             orders={ordersByStatus['confirmed']}
             details={t('Aqui você verá os novos pedidos. Aceite-os para confirmar o preparo.')}
           />
-          {/*<OrdersKanbanList
-          title={t('Confirmados')}
-          orders={ordersByStatus['confirmed']}
-          details={t(
-            'Aqui você verá os pedidos confirmados que ainda não começaram a ser preparados.'
-          )}
-          />*/}
           <OrdersKanbanList
             title={t('Em preparação')}
             orders={ordersByStatus['preparing']}
