29fbcb991245c6716bdff94d3f7271723f1d81a9
RenanC <renancostam@gmail.com>
2021-06-17 17:51:37 -0300

    useOrderDeliveryInfos isMatched fix

--- src/app/api/order/useOrderDeliveryInfos.ts
+++ src/app/api/order/useOrderDeliveryInfos.ts
@@ -32,7 +32,7 @@ export const useOrderDeliveryInfos = (order?: WithId<Order> | null) => {
     if (!order?.dispatchingStatus) return;
     const getOrderDispatchingText = (status: DispatchingStatus, state?: DispatchingState) => {
       let result = 'Buscando informações...';
-      if (status === 'matched') {
+      if (status === 'matched' || status === 'confirmed') {
         result = 'Buscando entregador';
         if (state === 'going-pickup') result = 'Entregador a caminho da retirada';
         if (state === 'arrived-pickup') result = 'Entregador no local';
@@ -48,7 +48,7 @@ export const useOrderDeliveryInfos = (order?: WithId<Order> | null) => {
   React.useEffect(() => {
     if (!order?.dispatchingStatus) return;
     setIsMatching(order.dispatchingStatus === 'matching');
-    setIsMatched(order.dispatchingStatus === 'matched');
+    setIsMatched(order.dispatchingStatus === 'matched' || status === 'confirmed');
     setIsNoMatch(order.dispatchingStatus === 'no-match');
   }, [order?.dispatchingStatus]);
 
