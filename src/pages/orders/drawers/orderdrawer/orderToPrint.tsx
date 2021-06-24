91c874cf4fcf6e869ec5a97e4d80c0a25e84b662
RenanC <renancostam@gmail.com>
2021-06-24 17:40:11 -0300

    OrderToPrint layout adjustments

--- src/pages/orders/drawers/orderdrawer/orderToPrint.tsx
+++ src/pages/orders/drawers/orderdrawer/orderToPrint.tsx
@@ -1,7 +1,20 @@
-import { Box, Flex, Image, Table, Tbody, Td, Text, Tfoot, Th, Thead, Tr } from '@chakra-ui/react';
-import * as cpfutils from '@fnando/cpf';
+import {
+  Box,
+  Flex,
+  HStack,
+  Image,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Tfoot,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react';
 import { useBusinessProfile } from 'app/api/business/profile/useBusinessProfile';
 import { Order, OrderItem, WithId } from 'appjusto-types';
+import logoAppjusto from 'common/img/logo.svg';
 import React from 'react';
 import { formatCurrency } from 'utils/formatters';
 import { getDateAndHour } from 'utils/functions';
@@ -20,11 +33,19 @@ export const OrderToPrint = React.forwardRef<HTMLDivElement, OrderToPrintProps>(
     return (
       <Box ref={ref} maxW="300px" p="4" position="absolute" top="0" left="0" zIndex="-999">
         <Flex flexDir="column" alignItems="center">
-          <Box maxW="60px" borderRadius="30px" overflow="hidden">
-            <Image src={logo ?? ''} />
-          </Box>
-          <Text mt="2" color="black" fontSize="md" fontWeight="700" lineHeight="lg">
+          <HStack spacing={2} alignItems="center">
+            <Box maxW="30px" borderRadius="15px" overflow="hidden">
+              <Image src={logo ?? ''} />
+            </Box>
+            <Box maxW="40px" borderRadius="20px" overflow="hidden">
+              <Image src={logoAppjusto} />
+            </Box>
+          </HStack>
+          <Text mt="1" color="black" fontSize="xs" fontWeight="700" lineHeight="14px">
             {businessName}
+          </Text>
+          <Text mt="0" fontSize="10px">
+            {t('Por um delivery mais justo e transparente!')}
           </Text>
         </Flex>
         <Text mt="4" color="black" fontSize="2xl" fontWeight="700" lineHeight="28px" mb="2">
@@ -36,14 +57,6 @@ export const OrderToPrint = React.forwardRef<HTMLDivElement, OrderToPrintProps>(
             {order?.consumer?.name ?? 'N/E'}
           </Text>
         </Text>
-        {order?.consumer.cpf && (
-          <Text fontSize="12px" color="gray.600" fontWeight="500" lineHeight="16px">
-            {t('CPF:')}{' '}
-            <Text as="span" color="black" fontWeight="700">
-              {cpfutils.format(order.consumer.cpf)}
-            </Text>
-          </Text>
-        )}
         <Text fontSize="12px" color="gray.600" fontWeight="500" lineHeight="16px">
           {t('Hora:')}{' '}
           <Text as="span" color="black" fontWeight="700">
@@ -77,27 +90,29 @@ export const OrderToPrint = React.forwardRef<HTMLDivElement, OrderToPrintProps>(
             {order?.items?.map((item: OrderItem) => (
               <React.Fragment key={Math.random()}>
                 <Tr color="black" fontWeight="700">
-                  <Td fontSize="12px">
+                  <Td fontSize="13px">
                     {item.product.name} <br />
                     <Text as="span" fontSize="11px" fontWeight="500">
                       {item.notes}
                     </Text>
                   </Td>
-                  <Td isNumeric fontSize="12px">
+                  <Td isNumeric fontSize="13px">
                     {item.quantity}
                   </Td>
-                  <Td isNumeric fontSize="12px">
+                  <Td isNumeric fontSize="13px">
                     {formatCurrency(item.product.price)}
                   </Td>
                 </Tr>
                 {item.complements &&
                   item.complements.map((complement) => (
-                    <Tr key={Math.random()}>
-                      <Td fontSize="12px">{complement.name}</Td>
-                      <Td isNumeric fontSize="12px">
+                    <Tr key={Math.random()} mt="-20px" color="black">
+                      <Td fontSize="12px" fontWeight="500">
+                        {complement.name}
+                      </Td>
+                      <Td isNumeric fontSize="12px" fontWeight="500">
                         {item.quantity}
                       </Td>
-                      <Td isNumeric fontSize="12px">
+                      <Td isNumeric fontSize="12px" fontWeight="500">
                         {formatCurrency(complement.price)}
                       </Td>
                     </Tr>
@@ -118,12 +133,11 @@ export const OrderToPrint = React.forwardRef<HTMLDivElement, OrderToPrintProps>(
         <SectionTitle mt="2" fontSize="18px">
           {t('Observações')}
         </SectionTitle>
-        {order?.additionalInfo && (
+        {order?.additionalInfo ? (
           <Text mt="1" fontSize="12px">
             {order?.additionalInfo}
           </Text>
-        )}
-        {!order?.consumer.cpf && !order?.additionalInfo && (
+        ) : (
           <Text mt="1" fontSize="12px">
             {t('Sem observações.')}
           </Text>
