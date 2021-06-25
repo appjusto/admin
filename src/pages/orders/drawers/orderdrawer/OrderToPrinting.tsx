413a6a52a6a25dd0d30b86220958043a698a1448
RenanC <renancostam@gmail.com>
2021-06-25 12:44:53 -0300

    Finish order printing

--- src/pages/orders/drawers/orderdrawer/OrderToPrinting.tsx
+++ src/pages/orders/drawers/orderdrawer/OrderToPrinting.tsx
@@ -14,7 +14,7 @@ import {
 } from '@chakra-ui/react';
 import { useBusinessProfile } from 'app/api/business/profile/useBusinessProfile';
 import { Order, OrderItem, WithId } from 'appjusto-types';
-import logoAppjusto from 'common/img/logo.svg';
+import logoAppjusto from 'common/img/logo-black.svg';
 import React from 'react';
 import { formatCurrency } from 'utils/formatters';
 import { getDateAndHour } from 'utils/functions';
@@ -31,88 +31,115 @@ export const OrderToPrinting = React.forwardRef<HTMLDivElement, OrderToPrintProp
     const { logo } = useBusinessProfile();
     // UI - Hidden
     return (
-      <Box ref={ref} maxW="300px" p="4" position="absolute" top="0" left="0" zIndex="-999">
+      <Box
+        ref={ref}
+        maxW="300px"
+        p="4"
+        position="absolute"
+        top="0"
+        left="0"
+        color="black"
+        zIndex="-999"
+      >
         <Flex flexDir="column" alignItems="center">
           <HStack spacing={2} alignItems="center">
-            <Box maxW="30px" borderRadius="15px" overflow="hidden">
+            <Box maxW="60px">
               <Image src={logo ?? ''} />
             </Box>
-            <Box maxW="40px" borderRadius="20px" overflow="hidden">
+            <Box maxW="80px">
               <Image src={logoAppjusto} />
             </Box>
           </HStack>
-          <Text mt="1" color="black" fontSize="xs" fontWeight="700" lineHeight="14px">
+          <Text mt="1" fontSize="xs" fontWeight="700" lineHeight="14px">
             {businessName}
           </Text>
-          <Text mt="0" fontSize="10px">
+          <Text mt="0" fontSize="11px">
             {t('Por um delivery mais justo e transparente!')}
           </Text>
         </Flex>
-        <Text mt="4" color="black" fontSize="2xl" fontWeight="700" lineHeight="28px" mb="2">
+        <Text mt="4" fontSize="2xl" fontWeight="700" lineHeight="28px" mb="2">
           {t('Pedido Nº')} {order?.code}
         </Text>
-        <Text fontSize="12px" color="gray.600" fontWeight="500" lineHeight="16px">
+        <Text fontSize="12px" fontWeight="500" lineHeight="16px">
           {t('Cliente:')}{' '}
-          <Text as="span" color="black" fontWeight="700">
+          <Text as="span" fontWeight="700">
             {order?.consumer?.name ?? 'N/E'}
           </Text>
         </Text>
-        <Text fontSize="12px" color="gray.600" fontWeight="500" lineHeight="16px">
+        <Text fontSize="12px" fontWeight="500" lineHeight="16px">
           {t('Hora:')}{' '}
-          <Text as="span" color="black" fontWeight="700">
+          <Text as="span" fontWeight="700">
             {getDateAndHour(order?.confirmedOn)}
           </Text>
         </Text>
-        <Text fontSize="12px" color="gray.600" fontWeight="500" lineHeight="16px">
+        <Text fontSize="12px" fontWeight="500" lineHeight="16px">
           {t('Endereço:')}{' '}
-          <Text as="span" color="black" fontWeight="700">
+          <Text as="span" fontWeight="700">
             {order?.destination?.address.main ?? 'N/E'}
           </Text>
         </Text>
-        <Text fontSize="12px" color="gray.600" fontWeight="500" lineHeight="16px">
+        <Text fontSize="12px" fontWeight="500" lineHeight="16px">
           {t('Complemento:')}{' '}
-          <Text as="span" color="black" fontWeight="700">
+          <Text as="span" fontWeight="700">
             {order?.destination?.additionalInfo ?? 'N/I'}
           </Text>
         </Text>
         <SectionTitle mt="2" fontSize="18px">
           {t('Detalhes do pedido')}
         </SectionTitle>
-        <Table mt="2" size="sm" variant="simple">
-          <Thead>
+        <Table mt="2" size="sm" variant="unstyled" color="black">
+          <Thead borderBottom="1px solid black">
             <Tr>
-              <Th>{t('Item')}</Th>
-              <Th isNumeric>{t('Qtd.')}</Th>
-              <Th isNumeric>{t('Valor')}</Th>
+              <Th fontSize="12px" pl="0">
+                {t('Item')}
+              </Th>
+              <Th isNumeric fontSize="12px" px="0">
+                {t('Qtd.')}
+              </Th>
+              <Th isNumeric fontSize="12px" pr="0">
+                {t('Valor')}
+              </Th>
             </Tr>
           </Thead>
           <Tbody>
             {order?.items?.map((item: OrderItem) => (
               <React.Fragment key={Math.random()}>
-                <Tr color="black" fontWeight="700">
-                  <Td fontSize="13px">
+                <Tr
+                  fontWeight="700"
+                  borderBottom={
+                    item.complements && item.complements?.length > 0 ? 'none' : '1px solid black'
+                  }
+                >
+                  <Td fontSize="11px" pl="0">
                     {item.product.name} <br />
-                    <Text as="span" fontSize="11px" fontWeight="500">
+                    <Text as="span" fontSize="10px" fontWeight="500">
                       {item.notes}
                     </Text>
                   </Td>
-                  <Td isNumeric fontSize="13px">
+                  <Td isNumeric fontSize="11px" px="0">
                     {item.quantity}
                   </Td>
-                  <Td isNumeric fontSize="13px">
+                  <Td isNumeric fontSize="11px" pr="0">
                     {formatCurrency(item.product.price)}
                   </Td>
                 </Tr>
                 {item.complements &&
-                  item.complements.map((complement) => (
-                    <Tr key={Math.random()} mt="-20px" color="black">
-                      <Td fontSize="12px" fontWeight="500">
+                  item.complements.map((complement, index) => (
+                    <Tr
+                      key={Math.random()}
+                      mt="-20px"
+                      pl="0"
+                      borderBottom={
+                        item.complements?.length === index + 1 ? '1px solid black' : 'none'
+                      }
+                    >
+                      <Td fontSize="11px" fontWeight="500">
                         {complement.name}
                       </Td>
-                      <Td isNumeric fontSize="12px" fontWeight="500">
+                      <Td isNumeric fontSize="11px" fontWeight="500" px="0">
                         {item.quantity}
                       </Td>
-                      <Td isNumeric fontSize="12px" fontWeight="500">
+                      <Td isNumeric fontSize="11px" fontWeight="500" pr="0">
                         {formatCurrency(complement.price)}
                       </Td>
                     </Tr>
@@ -121,10 +148,12 @@ export const OrderToPrinting = React.forwardRef<HTMLDivElement, OrderToPrintProp
             ))}
           </Tbody>
           <Tfoot bgColor="gray.50">
-            <Tr color="black">
-              <Th>{t('Total')}</Th>
+            <Tr>
+              <Th fontSize="12px" pl="0">
+                {t('Total')}
+              </Th>
               <Th></Th>
-              <Th isNumeric>
+              <Th isNumeric fontSize="12px" pr="0">
                 {order?.fare?.business?.value ? formatCurrency(order.fare.business.value) : 0}
               </Th>
             </Tr>
@@ -134,7 +163,7 @@ export const OrderToPrinting = React.forwardRef<HTMLDivElement, OrderToPrintProp
           {t('Observações')}
         </SectionTitle>
         {order?.additionalInfo ? (
-          <Text mt="1" fontSize="12px">
+          <Text mt="1" fontSize="12px" fontWeight="700">
             {order?.additionalInfo}
           </Text>
         ) : (
