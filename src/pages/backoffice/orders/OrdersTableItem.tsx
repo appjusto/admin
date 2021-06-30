2ff09260aadd320f1a7136263d577a2a3b169882
RenanC <renancostam@gmail.com>
2021-06-30 16:07:58 -0300

    Starting point to calculate total order value

--- src/pages/backoffice/orders/OrdersTableItem.tsx
+++ src/pages/backoffice/orders/OrdersTableItem.tsx
@@ -13,8 +13,24 @@ interface ItemProps {
 export const OrdersTableItem = ({ order }: ItemProps) => {
   // context
   const { path } = useRouteMatch();
+  const isBackoffice = path.includes('backoffice');
   // helpers
-  const total = formatCurrency(Number(order.totalOrder));
+  const getFoodOrderTotal = (type: string) => {
+    let total = 0;
+    if (type === 'food') {
+      if (!isBackoffice) total = order.businessValue;
+      else {
+        if (!order.courierValue || !order.businessValue) return 'N/E';
+        total = order.courierValue + order.businessValue;
+      }
+    } else {
+      return 'N/E';
+      //if(!order.courierValue || !order.platformValue) return 'N/E';
+      //total = order.courierValue + order.platformValue;
+    }
+    return formatCurrency(total);
+  };
+  const total = getFoodOrderTotal(order.type);
   // UI
   return (
     <Tr key={order.objectID} color="black" fontSize="15px" lineHeight="21px">
