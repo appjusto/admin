4cde7a773d0be59530ab21e81abec73544902e9e
RenanC <renancostam@gmail.com>
2021-07-02 18:34:44 -0300

    Add invoice external id

--- src/pages/backoffice/invoices/InvoicesTableItem.tsx
+++ src/pages/backoffice/invoices/InvoicesTableItem.tsx
@@ -12,7 +12,7 @@ export const InvoicesTableItem = ({ invoice }: ItemProps) => {
   // UI
   return (
     <Tr color="black" fontSize="15px" lineHeight="21px">
-      <Td>{invoice.id ?? 'N/I'}</Td>
+      <Td>{invoice.accountExternalId ?? 'N/I'}</Td>
       <Td>{getDateAndHour(invoice.createdOn)}</Td>
       <Td>{invoice.status ? invoiceStatusPTOptions[invoice.status] : 'N/E'}</Td>
       <Td>{invoice.invoiceType ? invoiceTypePTOptions[invoice.invoiceType] : 'N/E'}</Td>
