47c7869ca857e4c0a9500ff561187f644923b23d
RenanC <renancostam@gmail.com>
2021-07-06 14:10:35 -0300

    Highlight order issues in backoffice dashboard

---
+++ src/app/api/order/useObserveOrderIssues.ts
@@ -0,0 +1,18 @@
+import { useContextApi } from 'app/state/api/context';
+import { OrderIssue, WithId } from 'appjusto-types';
+import React from 'react';
+
+export const useObserveOrderIssues = (orderId?: string) => {
+  // context
+  const api = useContextApi();
+  // state
+  const [orderIssues, setOrderIssues] = React.useState<WithId<OrderIssue>[] | null>();
+  // side effects
+  React.useEffect(() => {
+    if (!orderId) return;
+    const unsub = api.order().observeOrderIssues(orderId, setOrderIssues);
+    return () => unsub();
+  }, [api, orderId]);
+  // return
+  return orderIssues;
+};
