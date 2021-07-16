38c8b328d3c1964273503ea4da2143a5967428db
RenanC <renancostam@gmail.com>
2021-07-16 15:47:31 -0300

    ManagerProfilePage: hide bankingInformation when no manager

--- src/pages/manager-profile/ManagerProfilePage.tsx
+++ src/pages/manager-profile/ManagerProfilePage.tsx
@@ -1,14 +1,20 @@
 import { Box } from '@chakra-ui/react';
+import { useFirebaseUserRole } from 'app/api/auth/useFirebaseUserRole';
 import BankingInformation from 'pages/business-profile/BankingInformation';
 import { ManagerProfile } from './ManagerProfile';
 
 const ManagerProfilePage = () => {
+  // context
+  const { role } = useFirebaseUserRole();
+  // UI
   return (
     <Box>
       <ManagerProfile />
-      <Box mt="8">
-        <BankingInformation />
-      </Box>
+      {role === 'manager' && (
+        <Box mt="8">
+          <BankingInformation />
+        </Box>
+      )}
     </Box>
   );
 };
