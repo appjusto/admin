0ba6da962f2eb1bfb6bd1bc70fb0ca582905e4b5
RenanC <renancostam@gmail.com>
2021-07-21 16:40:33 -0300

    Backoffice dashboard complete

--- src/pages/backoffice/dashboard/Panel.tsx
+++ src/pages/backoffice/dashboard/Panel.tsx
@@ -1,5 +1,6 @@
 import { Box, Flex, HStack, Skeleton, Text } from '@chakra-ui/react';
 import { useContextBackofficeDashboard } from 'app/state/dashboards/backoffice';
+import I18n from 'i18n-js';
 import React from 'react';
 import { formatCurrency } from 'utils/formatters';
 import { t } from 'utils/i18n';
@@ -44,9 +45,17 @@ export const Panel = () => {
     consumers,
   } = useContextBackofficeDashboard();
   // state
-
+  const [weekDay, setWeekDay] = React.useState<string>();
+  const [date, setDate] = React.useState<string>();
   // side effects
-
+  React.useEffect(() => {
+    const today = new Date();
+    const day = I18n.strftime(today, '%A').toLowerCase();
+    const dayName = ['sábado', 'domingo'].includes(day) ? day : `${day}-feira`;
+    const month = I18n.strftime(today, '%B').toLowerCase();
+    setWeekDay(dayName);
+    setDate(`${today.getDate()} de ${month} de ${today.getFullYear()}`);
+  }, []);
   // UI
   return (
     <Flex
@@ -60,10 +69,10 @@ export const Panel = () => {
     >
       <Box>
         <Text fontSize="2xl" fontWeight="700" lineHeight="28.8px">
-          {t('Hoje, quarta-feira')}
+          {t(`Hoje, ${weekDay}!`)}
         </Text>
         <Text mt="2" fontSize="sm" fontWeight="500" lineHeight="21px" color="green.600">
-          {t('10 de janeiro de 2020')}
+          {date}
         </Text>
         <HStack mt="4" spacing={6}>
           <BOInfoBox title={t('Pedidos')} value={todayOrders} />
