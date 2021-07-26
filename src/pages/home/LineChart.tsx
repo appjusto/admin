ccba2e3a23ee31054e40b0de06de78b542d2d4e6
RenanC <renancostam@gmail.com>
2021-07-26 15:42:26 -0300

    Using chart js only

--- src/pages/home/LineChart.tsx
+++ src/pages/home/LineChart.tsx
@@ -1,7 +1,7 @@
-import { Box, BoxProps } from '@chakra-ui/react';
-import I18n from 'i18n-js';
+import { Box, BoxProps, Center, Text } from '@chakra-ui/react';
+import { Chart, registerables } from 'chart.js';
+import I18n, { t } from 'i18n-js';
 import React from 'react';
-import { defaults, Line } from 'react-chartjs-2';
 
 interface LineChartProps extends BoxProps {
   currentWeekData: number[];
@@ -11,40 +11,11 @@ interface LineChartProps extends BoxProps {
 export const LineChart = ({ currentWeekData, lastWeekData, ...props }: LineChartProps) => {
   // state
   const [chartLabels, setChartLabels] = React.useState<string[]>();
+  const [isError, setIsError] = React.useState(false);
+  // refs
+  const chartCanvas = React.useRef<HTMLCanvasElement>(null);
   // chart
-  defaults.animation = false;
-  const data = {
-    labels: chartLabels,
-    datasets: [
-      {
-        label: 'Semana atual',
-        data: currentWeekData,
-        fill: false,
-        backgroundColor: '#4EA031',
-        borderColor: '#4EA031',
-      },
-      {
-        label: 'Semana anterior',
-        data: lastWeekData,
-        fill: false,
-        backgroundColor: '#C8D7CB',
-        borderColor: '#C8D7CB',
-      },
-    ],
-  };
-  const options = {
-    scales: {
-      yAxes: [
-        {
-          ticks: {
-            beginAtZero: true,
-            stepSize: 1,
-          },
-        },
-      ],
-    },
-    maintainAspectRatio: false,
-  };
+  Chart.register(...registerables);
   // side effects
   React.useEffect(() => {
     let today = new Date();
@@ -55,10 +26,69 @@ export const LineChart = ({ currentWeekData, lastWeekData, ...props }: LineChart
     }
     setChartLabels(labels.reverse());
   }, []);
+  React.useEffect(() => {
+    if (!chartCanvas.current) return;
+    if (!currentWeekData || !lastWeekData) return;
+    if (!chartLabels) return;
+    try {
+      setIsError(false);
+      let ctx = chartCanvas.current.getContext('2d');
+      const myChart = new Chart(ctx!, {
+        type: 'line',
+        data: {
+          labels: chartLabels,
+          datasets: [
+            {
+              label: 'Semana atual',
+              data: currentWeekData,
+              fill: false,
+              backgroundColor: '#4EA031',
+              borderColor: '#4EA031',
+            },
+            {
+              label: 'Semana anterior',
+              data: lastWeekData,
+              fill: false,
+              backgroundColor: '#C8D7CB',
+              borderColor: '#C8D7CB',
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: false,
+          scales: {
+            y: {
+              beginAtZero: true,
+              ticks: {
+                precision: 0,
+              },
+            },
+          },
+          plugins: {
+            legend: {
+              display: false,
+            },
+          },
+        },
+      });
+      return () => myChart.destroy();
+    } catch (error) {
+      // sentry
+      setIsError(true);
+    }
+  }, [chartLabels, currentWeekData, lastWeekData]);
+
   // UI
   return (
     <Box mt="4" position="relative" h="260px" {...props}>
-      <Line data={data} options={options} />
+      {isError ? (
+        <Center>
+          <Text>{t('Ocorreu um erro ao construir o gráfico =/')}</Text>
+        </Center>
+      ) : (
+        <canvas ref={chartCanvas} id="adminDashChart"></canvas>
+      )}
     </Box>
   );
 };
