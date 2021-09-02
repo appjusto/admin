c07bf7f91eec552d22ceea89a1cbaeaf1b3e4851
Pedro Andrade <pdandradeb@gmail.com>
2021-09-02 19:55:29 -0300

    Updating date reference

--- src/app/api/consumer/useObserveNewConsumers.ts
+++ src/app/api/consumer/useObserveNewConsumers.ts
@@ -1,20 +1,26 @@
 import { useContextApi } from 'app/state/api/context';
 import React from 'react';
+import dayjs from 'dayjs';
 
 export const useObserveNewConsumers = () => {
   // context
   const api = useContextApi();
   // state
   const [consumers, setConsumers] = React.useState<number>();
+  const [date, setDate] = React.useState(new Date());
   // side effects
   React.useEffect(() => {
-    const now = new Date();
-    const startDate = new Date(now.getFullYear(), now.getMonth(), now.getDate());
     const unsub = api.consumer().observeNewConsumers((consumers) => {
       setConsumers(consumers.length);
-    }, startDate);
+    }, dayjs(date).startOf('day').toDate());
     return () => unsub();
   }, [api]);
+  // update date reference
+  React.useEffect(() => {
+    const diff = dayjs(date).add(1, 'day').diff(date);
+    const interval = setTimeout(() => setDate(new Date()), diff);
+    () => clearInterval(interval);
+  }, [date]);
   // return
   return consumers;
 };
