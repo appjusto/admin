ce380d8686a924c31344245dd41190eada9ef690
RenanC <renancostam@gmail.com>
2021-09-02 17:28:25 -0300

    Add expired to orderStatusPTOptions

--- src/pages/backoffice/utils/index.ts
+++ src/pages/backoffice/utils/index.ts
@@ -35,6 +35,7 @@ export const orderStatusPTOptions = {
   dispatching: 'A caminho da entrega',
   delivered: 'Entregue',
   canceled: 'Cancelado',
+  expired: 'Expirado',
 };
 
 export const orderStatusPTOptionsForTableItem = {
@@ -47,6 +48,7 @@ export const orderStatusPTOptionsForTableItem = {
   dispatching: 'Despachando',
   delivered: 'Entregue',
   canceled: 'Cancelado',
+  expired: 'Expirado',
 };
 
 export const orderDispatchingStatusPTOptions = {
