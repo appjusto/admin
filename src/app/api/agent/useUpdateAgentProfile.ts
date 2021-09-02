0439b2c7a4ff297970ed80b8e66c6d5a219758fe
RenanC <renancostam@gmail.com>
2021-09-02 12:03:51 -0300

    Agents with password login

--- src/app/api/agent/useUpdateAgentProfile.ts
+++ src/app/api/agent/useUpdateAgentProfile.ts
@@ -2,15 +2,26 @@ import { useContextAgentProfile } from 'app/state/agent/context';
 import { useContextApi } from 'app/state/api/context';
 import { ManagerProfile } from 'appjusto-types';
 import { useMutation } from 'react-query';
+import { useAuthentication } from '../auth/useAuthentication';
+
+interface UpdateAgentData {
+  changes: Partial<ManagerProfile>;
+  password?: string;
+  currentPassword?: string;
+}
 
 export const useUpdateAgentProfile = () => {
   // context
   const api = useContextApi();
+  const { updateUsersPassword } = useAuthentication();
   const { agent } = useContextAgentProfile();
   // mutations
-  const [updateProfile, updateResult] = useMutation(async (changes: Partial<ManagerProfile>) =>
-    api.manager().updateProfile(agent?.id!, changes)
-  );
+  const [updateProfile, updateResult] = useMutation(async (data: UpdateAgentData) => {
+    if (data.password) {
+      await updateUsersPassword(data.password, data.currentPassword);
+    }
+    return api.manager().updateProfile(agent?.id!, data.changes);
+  });
   // return
   return { updateProfile, updateResult };
 };
--- src/pages/backoffice/agent/AgentProfile.tsx
+++ src/pages/backoffice/agent/AgentProfile.tsx
@@ -1,10 +1,13 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Button, Heading, Text } from '@chakra-ui/react';
 import * as cpfutils from '@fnando/cpf';
 import { useUpdateAgentProfile } from 'app/api/agent/useUpdateAgentProfile';
+import { useAuthentication } from 'app/api/auth/useAuthentication';
 import { useContextAgentProfile } from 'app/state/agent/context';
+import { AlertSuccess } from 'common/components/AlertSuccess';
 import { SuccessAndErrorHandler } from 'common/components/error/SuccessAndErrorHandler';
 import { initialError } from 'common/components/error/utils';
 import { CustomInput } from 'common/components/form/input/CustomInput';
+import { CustomPasswordInput } from 'common/components/form/input/CustomPasswordInput';
 import { CustomPatternInput } from 'common/components/form/input/pattern-input/CustomPatternInput';
 import {
   cpfFormatter,
@@ -21,6 +24,7 @@ import { t } from 'utils/i18n';
 export const AgentProfile = () => {
   // context
   const { agent } = useContextAgentProfile();
+  const { sendSignInLinkToEmail, sendingLinkResult } = useAuthentication();
   const { updateProfile, updateResult } = useUpdateAgentProfile();
   const { isLoading, isSuccess, isError, error: updateError } = updateResult;
 
@@ -30,17 +34,36 @@ export const AgentProfile = () => {
   const [phoneNumber, setPhoneNumber] = React.useState(agent?.phone ?? '');
   const [cpf, setCPF] = React.useState(agent?.cpf ?? '');
   const [error, setError] = React.useState(initialError);
+  const [isEditingPasswd, setIsEditingPasswd] = React.useState(true);
+  const [passwd, setPasswd] = React.useState('');
+  const [passwdConfirm, setPasswdConfirm] = React.useState('');
+  const [passwdIsValid, setPasswdIsValid] = React.useState(false);
+  const [currentPasswd, setCurrentPasswd] = React.useState('');
 
   // refs
   const submission = React.useRef(0);
   const nameRef = React.useRef<HTMLInputElement>(null);
   const cpfRef = React.useRef<HTMLInputElement>(null);
   const phoneNumberRef = React.useRef<HTMLInputElement>(null);
+  const passwdRef = React.useRef<HTMLInputElement>(null);
+  const passwdConfirmRef = React.useRef<HTMLInputElement>(null);
+  const currentPasswdRef = React.useRef<HTMLInputElement>(null);
 
   // helpers
   const isCPFValid = () => cpfutils.isValid(cpf);
+  const isReauthenticationRequired = React.useCallback(() => {
+    if (updateError && JSON.stringify(updateError).includes('recent authentication')) return true;
+    else return false;
+  }, [updateError]);
 
   // handlers
+  const clearState = React.useCallback(() => {
+    setName('');
+    setSurname('');
+    setPhoneNumber('');
+    setCPF('');
+  }, []);
+
   const onSubmitHandler = async () => {
     submission.current += 1;
     setError(initialError);
@@ -60,31 +83,74 @@ export const AgentProfile = () => {
       });
       return phoneNumberRef?.current?.focus();
     }
-    await updateProfile({
-      name,
-      surname,
-      phone: phoneNumber,
-      cpf,
-    });
+    if (passwd) {
+      if (passwd !== passwdConfirm) {
+        setError({
+          status: true,
+          error: null,
+          message: { title: 'As senhas informadas não são iguais.' },
+        });
+        return passwdRef?.current?.focus();
+      }
+      if (!passwdIsValid) {
+        setError({
+          status: true,
+          error: null,
+          message: { title: 'A senha informada não é válida.' },
+        });
+        return passwdRef?.current?.focus();
+      }
+      const data = {
+        changes: {
+          name,
+          surname,
+          phone: phoneNumber,
+          cpf,
+          isPasswordActive: true,
+        },
+        password: passwd,
+        currentPassword: currentPasswd,
+      };
+      await updateProfile(data);
+    } else {
+      const data = {
+        changes: {
+          name,
+          surname,
+          phone: phoneNumber,
+          cpf,
+          isPasswordActive: true,
+        },
+      };
+      await updateProfile(data);
+    }
   };
 
   // side effects
   React.useEffect(() => {
+    clearState();
     if (agent) {
       if (agent.name) setName(agent.name);
       if (agent.surname) setSurname(agent.surname);
       if (agent.phone) setPhoneNumber(agent.phone);
       if (agent.cpf) setCPF(agent.cpf);
+      if (agent.isPasswordActive) setIsEditingPasswd(false);
     }
-  }, [agent]);
+  }, [agent, clearState]);
 
   React.useEffect(() => {
-    if (isError)
-      setError({
-        status: true,
-        error: updateError,
-      });
-  }, [isError, updateError]);
+    if (isError) {
+      if (isReauthenticationRequired()) {
+        console.warn('User reauthentication required');
+        currentPasswdRef.current?.focus();
+      } else {
+        setError({
+          status: true,
+          error: updateError,
+        });
+      }
+    }
+  }, [isError, updateError, isReauthenticationRequired]);
 
   // UI
   return (
@@ -155,6 +221,142 @@ export const AgentProfile = () => {
           onValueChange={(value) => setCPF(value)}
           externalValidation={{ active: true, status: isCPFValid() }}
         />
+        {isEditingPasswd ? (
+          <>
+            <Heading mt="8" color="black" fontSize="xl">
+              {agent?.isPasswordActive ? t('Alterar senha') : t('Senha de acesso')}
+            </Heading>
+            <Text mt="1" fontSize="sm" maxW="580px">
+              {t(
+                'Se preferir, você pode definir uma senha de acesso a plataforma. Quem estiver com o login e senha não precisará do link de confirmação enviado por e-mail.'
+              )}
+            </Text>
+            {isReauthenticationRequired() ? (
+              agent?.isPasswordActive ? (
+                <>
+                  <Text
+                    mt="4"
+                    p="2"
+                    fontSize="sm"
+                    maxW="580px"
+                    bg="#FFFFCC"
+                    border="1px solid #FFBE00"
+                    borderRadius="lg"
+                  >
+                    {t(
+                      'Como já faz algum tempo desde o seu último login, é preciso informar a sua senha atual, para prosseguir.'
+                    )}
+                  </Text>
+                  <CustomPasswordInput
+                    ref={currentPasswdRef}
+                    mt="2"
+                    id="manager-current-password"
+                    label={t('Senha atual')}
+                    placeholder={t('Digite a sua senha atual')}
+                    value={currentPasswd}
+                    handleChange={(ev) => setCurrentPasswd(ev.target.value)}
+                    getValidity={setPasswdIsValid}
+                  />
+                  <Text mt="4" fontSize="xs" maxW="580px">
+                    {t(
+                      'A senha precisará ter no mínimo 8 caracteres, com pelo menos uma letra maíuscula e um número.'
+                    )}
+                  </Text>
+                  <CustomPasswordInput
+                    ref={passwdRef}
+                    mt="2"
+                    id="manager-password"
+                    label={t('Senha de acesso')}
+                    placeholder={t('Digite uma senha')}
+                    value={passwd}
+                    handleChange={(ev) => setPasswd(ev.target.value)}
+                    getValidity={setPasswdIsValid}
+                  />
+                  <CustomPasswordInput
+                    ref={passwdConfirmRef}
+                    isRequired={passwd ? true : false}
+                    isDisabled={!passwd ? true : false}
+                    id="manager-password-confirmation"
+                    label={t('Confirmar senha')}
+                    placeholder={t('Digite a senha novamente')}
+                    value={passwdConfirm}
+                    handleChange={(ev) => setPasswdConfirm(ev.target.value)}
+                  />
+                </>
+              ) : (
+                <Box mb="8">
+                  <Text
+                    mt="4"
+                    p="2"
+                    fontSize="sm"
+                    maxW="580px"
+                    bg="#FFFFCC"
+                    border="1px solid #FFBE00"
+                    borderRadius="lg"
+                  >
+                    {t(
+                      'Como já faz algum tempo desde o seu último login, e ainda não há senha cadastrada, é preciso realizar um novo login (por meio do link de acesso que é enviado ao seu e-mail) antes de cadastrar sua senha. Deseja enviar o link de acesso?'
+                    )}
+                  </Text>
+                  <Button
+                    mt="4"
+                    w="100%"
+                    onClick={() => sendSignInLinkToEmail(agent?.email)}
+                    isLoading={sendingLinkResult.isLoading}
+                  >
+                    {t('Enviar link de acesso')}
+                  </Button>
+                  {sendingLinkResult.isSuccess && (
+                    <AlertSuccess
+                      title={t('Pronto!')}
+                      description={t('O link de acesso foi enviado para seu e-mail.')}
+                    />
+                  )}
+                </Box>
+              )
+            ) : (
+              <>
+                <Text mt="4" fontSize="xs" maxW="580px">
+                  {t(
+                    'A senha precisará ter no mínimo 8 caracteres, com pelo menos uma letra maíuscula e um número.'
+                  )}
+                </Text>
+                <CustomPasswordInput
+                  ref={passwdRef}
+                  mt="2"
+                  id="manager-password"
+                  label={t('Senha de acesso')}
+                  placeholder={t('Digite uma senha')}
+                  value={passwd}
+                  handleChange={(ev) => setPasswd(ev.target.value)}
+                  getValidity={setPasswdIsValid}
+                />
+                <CustomPasswordInput
+                  ref={passwdConfirmRef}
+                  isRequired={passwd ? true : false}
+                  isDisabled={!passwd ? true : false}
+                  id="manager-password-confirmation"
+                  label={t('Confirmar senha')}
+                  placeholder={t('Digite a senha novamente')}
+                  value={passwdConfirm}
+                  handleChange={(ev) => setPasswdConfirm(ev.target.value)}
+                />
+              </>
+            )}
+          </>
+        ) : (
+          <Text mt="6" color="black">
+            <Text
+              w="auto"
+              as="span"
+              textDecor="underline"
+              cursor="pointer"
+              onClick={() => setIsEditingPasswd(true)}
+            >
+              {t('Editar senha de acesso')}
+            </Text>
+          </Text>
+        )}
         <PageFooter isLoading={isLoading} />
       </form>
     </Box>
