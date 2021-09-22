b21f44b9bc908b657513d803346969dd38a57da7
RenanC <renancostam@gmail.com>
2021-09-22 17:40:09 -0300

    Send error to Sentry in useAccountInformation

--- src/app/api/business/useAccountInformation.ts
+++ src/app/api/business/useAccountInformation.ts
@@ -1,6 +1,7 @@
 import { useContextApi } from 'app/state/api/context';
 import { FetchAccountInformationResponse } from 'appjusto-types';
 import React from 'react';
+import * as Sentry from '@sentry/react';
 
 export const useAccountInformation = (businessId?: string) => {
   // context
@@ -9,13 +10,19 @@ export const useAccountInformation = (businessId?: string) => {
   const [
     accountInformation,
     setAccountInformation,
-  ] = React.useState<FetchAccountInformationResponse>();
+  ] = React.useState<FetchAccountInformationResponse | null>();
   // handlers
   const refreshAccountInformation = React.useCallback(() => {
     if (!businessId) return;
     (async () => {
-      const result = await api.business().fetchAccountInformation(businessId);
-      if (result) setAccountInformation(result);
+      try {
+        const result = await api.business().fetchAccountInformation(businessId);
+        if (result) setAccountInformation(result);
+        else setAccountInformation(null);
+      } catch (error) {
+        Sentry.captureException(error);
+        setAccountInformation(null);
+      }
     })();
   }, [api, businessId]);
   // side effects
