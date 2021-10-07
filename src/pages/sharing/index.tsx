77fc38c71fea815d197b93ff67666dba8d469427
RenanC <renancostam@gmail.com>
2021-10-07 15:29:54 -0300

    SharingPage layout adjust

--- src/pages/sharing/index.tsx
+++ src/pages/sharing/index.tsx
@@ -116,46 +116,48 @@ const SharingPage = () => {
         </Button>
       </Stack>
       {business?.slug && (
-        <Stack
-          mt="8"
-          direction="row"
-          spacing={4}
-          alignItems="center"
-          p="6"
-          bgColor="#F6F6F6"
-          borderRadius="lg"
-          color="black"
-        >
-          <Center p="2" bgColor="#fff" borderRadius="18px">
-            <Icon as={MdInfoOutline} w="24px" h="24px" />
-          </Center>
-          <Text maxW="847px" fontSize="16px" lineHeight="22px" fontWeight="500">
-            {t(
-              'Caso o cliente tenha o aplicativo do AppJusto instalado, poderá abrir os links abaixo diretamente no app ou no navegador padrão. Se abrir pelo navegador, ele verá o cardápio digital com a opção do link selecionado.'
-            )}
-          </Text>
-        </Stack>
+        <>
+          <Stack
+            mt="8"
+            direction="row"
+            spacing={4}
+            alignItems="center"
+            p="6"
+            bgColor="#F6F6F6"
+            borderRadius="lg"
+            color="black"
+          >
+            <Center p="2" bgColor="#fff" borderRadius="18px">
+              <Icon as={MdInfoOutline} w="24px" h="24px" />
+            </Center>
+            <Text maxW="847px" fontSize="16px" lineHeight="22px" fontWeight="500">
+              {t(
+                'Caso o cliente tenha o aplicativo do AppJusto instalado, poderá abrir os links abaixo diretamente no app ou no navegador padrão. Se abrir pelo navegador, ele verá o cardápio digital com a opção do link selecionado.'
+              )}
+            </Text>
+          </Stack>
+          <LinkBoxWithOption
+            id="QRCode1"
+            title="Cardápio com botão: fazer pedidos"
+            description="Ao clicar, seu cliente abrirá o cardápio com um botão de ação. Escolha a ação abaixo:"
+            mode="whatsapp"
+            copied={isCopied}
+            copy={(mode) => copyToClipboard(mode)}
+            getLink={(mode) => getBusinessLinkByMode(mode)}
+            getSharingMessage={(mode) => getWhatsappSharingMessage(mode)}
+          />
+          <LinkBox
+            id="QRCode2"
+            title="Cardápio sem botão: somente visualização"
+            description="Ao clicar, seu cliente abrirá a página do cardápio para visualização na loja, sem a opção de pedir"
+            mode="in-store"
+            copied={isCopied}
+            link={getBusinessLinkByMode('in-store')}
+            sharingMessage={getWhatsappSharingMessage('in-store')}
+            copy={() => copyToClipboard('in-store')}
+          />
+        </>
       )}
-      <LinkBoxWithOption
-        id="QRCode1"
-        title="Cardápio com botão: fazer pedidos"
-        description="Ao clicar, seu cliente abrirá o cardápio com um botão de ação. Escolha a ação abaixo:"
-        mode="whatsapp"
-        copied={isCopied}
-        copy={(mode) => copyToClipboard(mode)}
-        getLink={(mode) => getBusinessLinkByMode(mode)}
-        getSharingMessage={(mode) => getWhatsappSharingMessage(mode)}
-      />
-      <LinkBox
-        id="QRCode2"
-        title="Cardápio sem botão: somente visualização"
-        description="Ao clicar, seu cliente abrirá a página do cardápio para visualização na loja, sem a opção de pedir"
-        mode="in-store"
-        copied={isCopied}
-        link={getBusinessLinkByMode('in-store')}
-        sharingMessage={getWhatsappSharingMessage('in-store')}
-        copy={() => copyToClipboard('in-store')}
-      />
       <SuccessAndErrorHandler
         submission={submission.current}
         isSuccess={isSuccess}
