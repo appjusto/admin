e47b1179d5d326b9f963798df2e974d9d158c0ab
RenanC <renancostam@gmail.com>
2022-02-21 16:40:09 -0300

    useFirebaseUser sending user to Sentry

--- src/app/api/auth/useFirebaseUser.ts
+++ src/app/api/auth/useFirebaseUser.ts
@@ -1,3 +1,4 @@
+import * as Sentry from '@sentry/react';
 import { useContextApi } from 'app/state/api/context';
 import firebase from 'firebase/app';
 import React from 'react';
@@ -10,6 +11,8 @@ export const useFirebaseUser = () => {
   // side effects
   React.useEffect(() => {
     const unsub = api.auth().observeAuthState((user) => {
+      if (user) Sentry.setUser({ id: user.uid, email: user.email! });
+      else Sentry.configureScope((scope) => scope.setUser(null));
       setFirebaseUser(user);
     });
     return () => unsub();
