b8faa2d93f8b0f16ccea14473821672940b1f293
RenanC <renancostam@gmail.com>
2022-05-27 18:01:19 -0300

    Fix deleteFlaggedLocation flow UI

--- src/app/api/platform/useFlaggedLocations.ts
+++ src/app/api/platform/useFlaggedLocations.ts
@@ -10,23 +10,19 @@ export const useFlaggedLocations = () => {
   //  WithId<FlaggedLocation>[] | null
   //>();
   // mutations
-  const {
-    mutateAsync: addFlaggedLocation,
-    mutationResult: addFlaggedLocationResult,
-  } = useCustomMutation(
-    async (location: Partial<FlaggedLocation>) => api.platform().addFlaggedLocation(location),
-    'addFlaggedLocation',
-    false
-  );
-  const {
-    mutateAsync: deleteFlaggedLocation,
-    mutationResult: deleteFlaggedLocationResult,
-  } = useCustomMutation(
-    async (locationId: string) => api.platform().deleteFlaggedLocation(locationId),
-    'deleteFlaggedLocation',
-    false,
-    false
-  );
+  const { mutateAsync: addFlaggedLocation, mutationResult: addFlaggedLocationResult } =
+    useCustomMutation(
+      async (location: Partial<FlaggedLocation>) => api.platform().addFlaggedLocation(location),
+      'addFlaggedLocation',
+      false
+    );
+  const { mutateAsync: deleteFlaggedLocation, mutationResult: deleteFlaggedLocationResult } =
+    useCustomMutation(
+      async (locationId: string) => api.platform().deleteFlaggedLocation(locationId),
+      'deleteFlaggedLocation',
+      false,
+      false
+    );
   // side effects
   //React.useEffect(() => {
   //  if (!observe) return;
