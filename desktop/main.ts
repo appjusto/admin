e6b158ba676d0f870d2d61724d882f4b56a383ae
Renan Costa <renancostam@gmail.com>
2022-06-24 17:13:59 -0300

    Electron builder setup

--- desktop/main.ts
+++ desktop/main.ts
@@ -1,44 +1,67 @@
 import { app, BrowserWindow } from 'electron';
-import installExtension, { REACT_DEVELOPER_TOOLS } from "electron-devtools-installer";
 import * as path from 'path';
 
-function createWindow() {
-  const win = new BrowserWindow({
+const isDev = process.env.NODE_ENV === 'development';
+
+const isDebug = isDev || process.env.DEBUG_PROD === 'true';
+
+const installExtensions = async () => {
+  const installer = require('electron-devtools-installer');
+  const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
+  const extensions = ['REACT_DEVELOPER_TOOLS'];
+
+  return installer
+    .default(
+      extensions.map((name) => installer[name]),
+      forceDownload
+    )
+    .catch(console.log);
+};
+
+async function createWindow() {
+  if (isDebug) {
+    // DevTools
+    await installExtensions();
+  }
+
+  const mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
+    icon: path.join(__dirname, 'logo.png'),
     webPreferences: {
       // contextIsolation: false,
       preload: path.join(__dirname, 'preload.js')
     }
   })
-
-  if (app.isPackaged) {
-    // 'build/index.html'
-    win.loadURL(`file://${__dirname}/../index.html`);
-  } else {
-    win.loadURL('http://localhost:3000');
-
-    win.webContents.openDevTools();
-
-    // Hot Reloading on 'node_modules/.bin/electronPath'
-    // require('electron-reload')(__dirname, {
-    //   electron: path.join(__dirname,
-    //     '..',
-    //     '..',
-    //     'node_modules',
-    //     '.bin',
-    //     'electron' + (process.platform === "win32" ? ".cmd" : "")),
-    //   forceHardReset: true,
-    //   hardResetMethod: 'exit'
-    // });
+  console.log("NODE_ENV", process.env.NODE_ENV)
+  // const baseURL = isDev ? 'http://localhost:3000' : url.format({
+  //     pathname: path.join(__dirname, '../index.html'),
+  //     protocol: 'file:',
+  //     slashes: true
+  // });
+  // const baseURL = isDev ? 'http://localhost:3000' : `file://${__dirname}/../index.html`;
+  const baseURL = isDev ? 'http://localhost:3000' : `https://dev.admin.appjusto.com.br`;
+  mainWindow.loadURL(baseURL);
+  // if (app.isPackaged) {
+  //   // 'build/index.html'
+  //   mainWindow.loadURL(`file://${__dirname}/../index.html`);
+  // } else {
+  //   mainWindow.loadURL('http://localhost:3000');
+  //   }
+  if(isDebug) {
+    mainWindow.webContents.openDevTools();
   }
+    // mainWindow.webContents.openDevTools();
 }
 
+
 app.whenReady().then(() => {
-  // DevTools
-  installExtension(REACT_DEVELOPER_TOOLS)
-    .then((name) => console.log(`Added Extension:  ${name}`))
-    .catch((err) => console.log('An error occurred: ', err));
+  // // DevTools
+  // if(isDebug) {
+  //   installExtension(REACT_DEVELOPER_TOOLS)
+  //     .then((name) => console.log(`Added Extension:  ${name}`))
+  //     .catch((err) => console.log('An error occurred: ', err));
+  // }
 
   createWindow();
 
