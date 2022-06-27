750a8ecd241be7af52f9cb5c0270d77ea4cc259f
Renan Costa <renancostam@gmail.com>
2022-06-27 10:48:46 -0300

    Fix isEmailValid regex

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -1,6 +1,6 @@
 const EMAIL_REGEX =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-const DOMAIN_REGEX = /.com\w/;
+const DOMAIN_REGEX = /\.com\w/;
 const COMMONLY_EMAIL_TYPOS = [
   'gnail.com',
   'gamil.com',
