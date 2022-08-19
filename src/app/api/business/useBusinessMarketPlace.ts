6b373dbc40e7d4e1760cbbec4c25890a06e42ea7
Renan Costa <renancostam@gmail.com>
2022-08-19 15:52:29 -0300

    Apply useUserCanReadEntity to businesses api

--- src/app/api/business/useBusinessMarketPlace.ts
+++ src/app/api/business/useBusinessMarketPlace.ts
@@ -1,13 +1,16 @@
-import { useContextApi } from 'app/state/api/context';
 import { MarketplaceAccountInfo } from '@appjusto/types';
+import { useContextApi } from 'app/state/api/context';
 import React from 'react';
+import { useUserCanReadEntity } from '../auth/useUserCanReadEntity';
 import { useCustomMutation } from '../mutation/useCustomMutation';
 
 export const useBusinessMarketPlace = (businessId: string) => {
   // context
   const api = useContextApi();
+  const userCanRead = useUserCanReadEntity('businesses');
   // state
-  const [marketPlace, setMarketPlace] = React.useState<MarketplaceAccountInfo | null>();
+  const [marketPlace, setMarketPlace] =
+    React.useState<MarketplaceAccountInfo | null>();
   // mutations
   const {
     mutateAsync: deleteMarketPlace,
@@ -18,9 +21,12 @@ export const useBusinessMarketPlace = (businessId: string) => {
   );
   // side effects
   React.useEffect(() => {
+    if (!userCanRead) return;
     if (!businessId) return;
-    return api.business().observeBusinessMarketPlace(businessId, setMarketPlace);
-  }, [api, businessId]);
+    return api
+      .business()
+      .observeBusinessMarketPlace(businessId, setMarketPlace);
+  }, [api, userCanRead, businessId]);
   // return
   return { marketPlace, deleteMarketPlace, deleteMarketPlaceResult };
 };
--- src/app/api/business/useObserveBusinessAdvances.ts
+++ src/app/api/business/useObserveBusinessAdvances.ts
@@ -1,7 +1,8 @@
-import { useContextApi } from 'app/state/api/context';
 import { AccountAdvance, WithId } from '@appjusto/types';
-import React from 'react';
+import { useContextApi } from 'app/state/api/context';
 import dayjs from 'dayjs';
+import React from 'react';
+import { useUserCanReadEntity } from '../auth/useUserCanReadEntity';
 
 export const useObserveBusinessAdvances = (
   businessId: string | undefined,
@@ -9,17 +10,21 @@ export const useObserveBusinessAdvances = (
 ) => {
   // context
   const api = useContextApi();
+  const userCanRead = useUserCanReadEntity('advances');
   // state
   const [advances, setAdvances] = React.useState<WithId<AccountAdvance>[]>();
   // side effects
   React.useEffect(() => {
+    if (!userCanRead) return;
     if (!businessId) return;
     if (!month) return;
     const start = dayjs(month).startOf('month').toDate();
     const end = dayjs(month).endOf('month').toDate();
-    const unsub = api.business().observeBusinessAdvances(businessId, start, end, setAdvances);
+    const unsub = api
+      .business()
+      .observeBusinessAdvances(businessId, start, end, setAdvances);
     return () => unsub();
-  }, [api, businessId, month]);
+  }, [api, userCanRead, businessId, month]);
   // return
   return advances;
 };
