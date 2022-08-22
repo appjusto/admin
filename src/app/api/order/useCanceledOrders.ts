5c6c0f723c3409ec271537d79188840d71ebdc3a
Renan Costa <renancostam@gmail.com>
2022-08-22 11:28:38 -0300

    Apply useUserCanReadEntity to orders api

--- src/app/api/order/useCanceledOrders.ts
+++ src/app/api/order/useCanceledOrders.ts
@@ -1,18 +1,23 @@
-import { useContextApi } from 'app/state/api/context';
 import { Order, WithId } from '@appjusto/types';
+import { useContextApi } from 'app/state/api/context';
 import React from 'react';
+import { useUserCanReadEntity } from '../auth/useUserCanReadEntity';
 
 export const useCanceledOrders = (businessId?: string) => {
   // context
   const api = useContextApi();
+  const userCanRead = useUserCanReadEntity('orders');
   // state
   const [orders, setOrders] = React.useState<WithId<Order>[]>([]);
   // side effects
   React.useEffect(() => {
+    if (!userCanRead) return;
     if (!businessId) return;
-    const unsub = api.order().observeBusinessCanceledOrders(setOrders, businessId);
+    const unsub = api
+      .order()
+      .observeBusinessCanceledOrders(setOrders, businessId);
     return () => unsub();
-  }, [businessId, api]); //attention to 'options' to avoid infinite renders
+  }, [api, userCanRead, businessId]); //attention to 'options' to avoid infinite renders
   // return
   return orders;
 };
--- src/app/api/order/useObserveOrders.ts
+++ src/app/api/order/useObserveOrders.ts
@@ -1,6 +1,7 @@
 import { Order, OrderStatus, WithId } from '@appjusto/types';
 import { useContextApi } from 'app/state/api/context';
 import React from 'react';
+import { useUserCanReadEntity } from '../auth/useUserCanReadEntity';
 import { Ordering } from './OrderApi';
 
 export const useObserveOrders = (
@@ -11,15 +12,17 @@ export const useObserveOrders = (
 ) => {
   // context
   const api = useContextApi();
+  const userCanRead = useUserCanReadEntity('orders');
   // state
   const [orders, setOrders] = React.useState<WithId<Order>[]>([]);
   // side effects
   React.useEffect(() => {
+    if (!userCanRead) return;
     const unsub = api
       .order()
       .observeOrders(statuses, setOrders, businessId, ordering, queryLimit);
     return () => unsub();
-  }, [api, statuses, businessId, ordering, queryLimit]);
+  }, [api, userCanRead, statuses, businessId, ordering, queryLimit]);
   // return
   return orders;
 };
