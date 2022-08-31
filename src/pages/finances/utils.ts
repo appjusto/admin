48f4ffbd7972b14e2cfc36107f5db31addf34958
Renan Costa <renancostam@gmail.com>
2022-08-31 17:57:47 -0300

    calculateAppJustoCosts and calculateIuguCosts adjustments

--- src/pages/finances/utils.ts
+++ src/pages/finances/utils.ts
@@ -25,10 +25,11 @@ export const calculateAppJustoCosts = (
   amount: number,
   invoices: WithId<Invoice>[]
 ) => {
+  console.log('amount', amount);
   const value = invoices.reduce((total, invoice) => {
     return (total += invoice.fare?.commission ?? 0);
   }, 0);
-  const fee = value / amount;
+  const fee = amount > 0 ? value / amount : 0;
   return { value, fee };
 };
 
@@ -40,6 +41,6 @@ export const calculateIuguValue = (invoices: WithId<Invoice>[]) => {
 };
 
 export const calculateIuguCosts = (amount: number, iuguValue: number) => {
-  const fee = iuguValue / amount;
+  const fee = amount > 0 ? iuguValue / amount : 0;
   return { value: iuguValue, fee };
 };
