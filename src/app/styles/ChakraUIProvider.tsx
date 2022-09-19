fa23321b2dc035dc9d0d869eb736eb2b04805853
Renan Costa <renancostam@gmail.com>
2022-09-19 15:11:32 -0300

    Chakra theme adjustments

--- src/app/styles/ChakraUIProvider.tsx
+++ src/app/styles/ChakraUIProvider.tsx
@@ -1,9 +1,11 @@
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import React from 'react';
 import { Button } from './components/Button';
+import Checkbox from './components/Checkbox';
 import { CustomInput } from './components/CustomInput';
 import { CustomTextarea } from './components/CustomTextarea';
 import { IconButton } from './components/IconButton';
+import Radio from './components/Radio';
 import { Select } from './components/Select';
 import Switch from './components/Switch';
 import { colors } from './foundations/colors';
@@ -23,12 +25,14 @@ const theme = extendTheme({
     },
   },
   components: {
+    Button,
+    Checkbox,
     CustomInput,
     CustomTextarea,
-    Select,
-    Button,
-    Switch,
     IconButton,
+    Radio,
+    Select,
+    Switch,
   },
 });
 
