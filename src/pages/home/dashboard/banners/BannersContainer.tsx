70ee0b6085c620a3ce9d10a2c450fcd7e145d124
Renan Costa <renancostam@gmail.com>
2022-10-18 15:13:50 -0300

    BannersContainer behavior complete

--- src/pages/home/dashboard/banners/BannersContainer.tsx
+++ src/pages/home/dashboard/banners/BannersContainer.tsx
@@ -30,51 +30,42 @@ interface BannersProps {
   banners?: WithId<Banner>[] | null;
 }
 
-// const slide = (start: number, width: number) => keyframes`
-//     from {transform: translateX(${start}px);}
-//     to {transform: translateX(-${start + width}px)}
-//   `;
-
 export const BannersContainer = ({ banners }: BannersProps) => {
   // state
   const [baseWidth, setBaseWidth] = React.useState(0);
   const [autoPlay, setAutoPlay] = React.useState(true);
+  const [actionEnabled, setActionEnabled] = React.useState(true);
   const [transition, setTransition] = React.useState('');
   const [translateValue, setTranslateValue] = React.useState<number>();
   // refs
   const sliderRef = React.useRef<HTMLDivElement>(null);
   // handlers
-  const setInitialtransition = React.useCallback(() => {
-    if (!banners || banners.length < 2) return;
-    if (!baseWidth) return;
-    const initialtranslate = baseWidth * banners.length;
-    if (
-      !translateValue ||
-      translateValue === 0 ||
-      translateValue === initialtranslate * 2
-    ) {
+  const initialize = React.useCallback(
+    (width: number) => {
+      if (!banners) return;
+      const initialtranslate = banners.length > 1 ? width * banners.length : 0;
       setTransition('');
       setTranslateValue(initialtranslate);
-    }
-  }, [banners, baseWidth, translateValue]);
+    },
+    [banners]
+  );
   const handleTranslate = React.useCallback(
-    (direction: 'left' | 'right', auto?: boolean) => {
+    (multiplier: number, auto?: boolean) => {
       if (!banners || banners.length < 2) return;
       if (!baseWidth) return;
       if (auto && !autoPlay) return;
+      if (!auto) {
+        setActionEnabled(false);
+        setTimeout(() => setActionEnabled(true), 2000);
+      }
+      const direction = baseWidth * multiplier;
       setTranslateValue((prev) => {
         if (!prev) return;
-        if (direction === 'right') {
-          return prev + baseWidth;
-        } else {
-          return prev - baseWidth;
-        }
+        return prev + direction;
       });
     },
     [banners, baseWidth, autoPlay]
   );
-  // console.log('baseWidth', baseWidth);
-  console.log('tValue', translateValue);
   React.useEffect(() => {
     const handleResizing = () => {
       if (typeof window !== 'undefined') {
@@ -84,39 +75,42 @@ export const BannersContainer = ({ banners }: BannersProps) => {
           document.body.clientWidth;
         const baseWidth = clientWidth > 1000 ? 980 : clientWidth - 32;
         setBaseWidth(baseWidth);
+        initialize(baseWidth);
       }
     };
     handleResizing();
     window.addEventListener('resize', handleResizing);
     return () => window.removeEventListener('resize', handleResizing);
-  }, []);
+  }, [initialize]);
   React.useEffect(() => {
+    if (!baseWidth) return;
     if (!banners) return;
     if (banners.length < 2) {
       setTranslateValue(0);
       return;
     }
-    if (!baseWidth) return;
     const initialtranslate = baseWidth * banners.length;
     const restart = () =>
       setTimeout(() => {
-        setInitialtransition();
+        setTransition('');
+        setTranslateValue(initialtranslate);
       }, 2000);
     if (!translateValue || translateValue === initialtranslate * 2) restart();
     else if (translateValue === 0) restart();
     else {
       setTransition('transform 2s ease');
     }
-  }, [banners, baseWidth, translateValue, transition, setInitialtransition]);
+  }, [baseWidth, banners, translateValue]);
   React.useEffect(() => {
     if (!banners || banners.length < 2) return;
     const interval = setInterval(() => {
-      handleTranslate('right', true);
-    }, 6000);
+      handleTranslate(1, true);
+    }, 4000);
     return () => clearInterval(interval);
   }, [banners, handleTranslate]);
   // UI
-  if (!banners || translateValue === undefined) return <Box />;
+  if (!banners || banners.length === 0 || translateValue === undefined)
+    return <Box />;
   return (
     <Box
       mt="6"
@@ -135,30 +129,32 @@ export const BannersContainer = ({ banners }: BannersProps) => {
       >
         {renderCarouselSection(banners, baseWidth)}
       </Flex>
-      {banners.length > 1 && (
-        <>
-          <Center pos="absolute" top="0" left="0" w="auto" h="100%">
-            <Icon
-              as={ChevronLeftIcon}
-              color="white"
-              w="32px"
-              h="32px"
-              cursor="pointer"
-              onClick={() => handleTranslate('left')}
-            />
-          </Center>
-          <Center pos="absolute" top="0" right="0" w="auto" h="100%">
-            <Icon
-              as={ChevronRightIcon}
-              color="white"
-              w="32px"
-              h="32px"
-              cursor="pointer"
-              onClick={() => handleTranslate('right')}
-            />
-          </Center>
-        </>
-      )}
+      <Center pos="absolute" top="0" left="0" w="32px" h="100%">
+        {banners.length > 1 && actionEnabled && (
+          <Icon
+            as={ChevronLeftIcon}
+            color="white"
+            w="32px"
+            h="32px"
+            cursor="pointer"
+            _hover={{ opacity: 0.8 }}
+            onClick={() => handleTranslate(-1)}
+          />
+        )}
+      </Center>
+      <Center pos="absolute" top="0" right="0" w="32px" h="100%">
+        {banners.length > 1 && actionEnabled && (
+          <Icon
+            as={ChevronRightIcon}
+            color="white"
+            w="32px"
+            h="32px"
+            cursor="pointer"
+            _hover={{ opacity: 0.8 }}
+            onClick={() => handleTranslate(1)}
+          />
+        )}
+      </Center>
     </Box>
   );
 };
