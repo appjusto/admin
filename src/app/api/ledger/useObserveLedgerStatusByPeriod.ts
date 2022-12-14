af353d0644578a50431b7eef52d825149957b4b0
Renan Costa <renancostam@gmail.com>
2022-12-14 15:46:55 -0300

    Finances PeriodTable with insurance amount

--- src/app/api/ledger/useObserveLedgerStatusByPeriod.ts
+++ src/app/api/ledger/useObserveLedgerStatusByPeriod.ts
@@ -18,7 +18,8 @@ export const useObserveLedgerStatusByPeriod = (
   const userCanRead = useUserCanReadEntity('invoices');
   // state
   const [entries, setEntries] = React.useState<WithId<LedgerEntry>[] | null>();
-  const [periodAmount, setPeriodAmount] = React.useState(0);
+  const [deliveryAmount, setDeliveryAmount] = React.useState(0);
+  const [insuranceAmount, setInsuranceAmount] = React.useState(0);
   const [iuguValue, setIuguValue] = React.useState(0);
   // side effects
   React.useEffect(() => {
@@ -41,12 +42,20 @@ export const useObserveLedgerStatusByPeriod = (
   }, [api, userCanRead, businessId, month, statuses]);
   React.useEffect(() => {
     if (!entries) return;
-    const amount = getLedgerEntriesTotalValue(entries);
-    const iugu = getLedgerEntriesIuguTotalValue(entries);
-    setPeriodAmount(amount);
+    const deliveryEntries = entries.filter(
+      (entry) => entry.operation === 'delivery'
+    );
+    const insuranceEntries = entries.filter(
+      (entry) => entry.operation === 'business-insurance'
+    );
+    const deliveryTotal = getLedgerEntriesTotalValue(deliveryEntries);
+    const iugu = getLedgerEntriesIuguTotalValue(deliveryEntries);
+    const insuranceTotal = getLedgerEntriesTotalValue(insuranceEntries);
+    setDeliveryAmount(deliveryTotal);
+    setInsuranceAmount(insuranceTotal);
     setIuguValue(iugu);
     // TODO: iugu costs
   }, [entries]);
   // return
-  return { periodAmount, iuguValue };
+  return { deliveryAmount, iuguValue, insuranceAmount };
 };
