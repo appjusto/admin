f6b48f4c34d155f13ed636e878cdcb9c1d302d7c
Renan Costa <renancostam@gmail.com>
2023-03-15 15:18:14 -0300

    BOAccessRoute adjustment

--- src/pages/backoffice/routes/BOAccessRoute.tsx
+++ src/pages/backoffice/routes/BOAccessRoute.tsx
@@ -1,6 +1,5 @@
 import { useContextFirebaseUser } from 'app/state/auth/context';
 import { Loading } from 'common/components/Loading';
-import React from 'react';
 import { Redirect, Route, RouteProps } from 'react-router-dom';
 import { isAccessGranted } from 'utils/access';
 
@@ -9,7 +8,8 @@ export const BOAccessRoute = (props: RouteProps) => {
   const { isBackofficeUser, backofficePermissions } = useContextFirebaseUser();
   const path = props.path as string;
   // redirects
-  if (!isBackofficeUser) return <Redirect to="/404" push />;
+  console.log('isBackofficeUser', isBackofficeUser);
+  if (isBackofficeUser === false) return <Redirect to="/404" push />;
   if (path && backofficePermissions) {
     if (
       path === '/backoffice' ||
