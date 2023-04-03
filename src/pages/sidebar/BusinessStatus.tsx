4e0788230875fc4bc4186845c0d3f257efbf80a0
Renan Costa <renancostam@gmail.com>
2023-04-03 12:17:47 -0300

    Enabled value handler in BusinessStatus

--- src/pages/sidebar/BusinessStatus.tsx
+++ src/pages/sidebar/BusinessStatus.tsx
@@ -1,17 +1,52 @@
-import { Circle, Flex, SquareProps, Text } from '@chakra-ui/react';
+import {
+  Center,
+  Circle,
+  HStack,
+  Icon,
+  SquareProps,
+  Text,
+  Tooltip,
+} from '@chakra-ui/react';
+import { useContextBusiness } from 'app/state/business/context';
 import { useOrdersContext } from 'app/state/order';
+import React from 'react';
+import { MdErrorOutline } from 'react-icons/md';
 import { t } from 'utils/i18n';
 
 export const BusinessStatus = (props: SquareProps) => {
   // context
+  const { business } = useContextBusiness();
   const { isBusinessOpen } = useOrdersContext();
+  // helpers
+  const color = React.useMemo(() => {
+    if (isBusinessOpen && business?.enabled) return 'green.500';
+    else return 'red';
+  }, [business?.enabled, isBusinessOpen]);
+  const label = React.useMemo(() => {
+    if (!business?.enabled) return 'Invisível';
+    if (isBusinessOpen) return 'Aberto agora';
+    else return 'Fechado';
+  }, [business?.enabled, isBusinessOpen]);
   // UI
   return (
-    <Flex mt="1" alignItems="center">
-      <Circle size="8px" bg={isBusinessOpen ? 'green.500' : 'red'} {...props} />
-      <Text fontSize="md" ml="2">
-        {isBusinessOpen ? t('Aberto agora') : t('Fechado')}
-      </Text>
-    </Flex>
+    <HStack mt="1" spacing={2} alignItems="center">
+      <Circle mt="0.5px" size="8px" bg={color} {...props} />
+      <Text fontSize="md">{t(label)}</Text>
+      {!business?.enabled && (
+        <Tooltip
+          placement="right"
+          bg="yellow"
+          color="black"
+          hasArrow
+          label={t(
+            'Seu restaurante não aparecerá para seus clientes. Para deixá-lo visível, vá até a seção de "visibilidade" no menu "operação" ou contate o administrador desta unidade.'
+          )}
+        >
+          <Center mt="0.5px">
+            <Icon w="16px" h="16px" cursor="pointer" as={MdErrorOutline} />
+          </Center>
+        </Tooltip>
+      )}
+    </HStack>
   );
 };
