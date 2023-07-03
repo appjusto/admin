3d05204e068286cf0150d86e8e083f51f7ba52af
Renan Costa <renancostam@gmail.com>
2023-07-03 18:47:12 -0300

    Logout link added to OnbFooter

--- src/pages/onboarding/OnbFooter.tsx
+++ src/pages/onboarding/OnbFooter.tsx
@@ -1,6 +1,7 @@
-import { Box, Link, Stack, Text } from '@chakra-ui/react';
+import { Box, Flex, Link, Stack, Text } from '@chakra-ui/react';
 import Container from 'common/components/Container';
 import { Link as RouterLink } from 'react-router-dom';
+import { t } from 'utils/i18n';
 
 const OnbFooter = () => {
   return (
@@ -13,33 +14,38 @@ const OnbFooter = () => {
       zIndex="9999"
     >
       <Container py="10">
-        <Stack
-          direction={{ base: 'column', md: 'row' }}
-          spacing={4}
-          color="white"
-          fontSize="sm"
-          fontWeight="500"
-        >
-          <Link
-            as={RouterLink}
-            to="/"
-            maxW="170px"
-            textDecor="underline"
-            _hover={{ color: 'gray.400' }}
+        <Flex flexDir={{ base: 'column', md: 'row'}} justifyContent="space-between">
+          <Stack
+            direction={{ base: 'column', md: 'row' }}
+            spacing={4}
+            color="white"
+            fontSize="sm"
+            fontWeight="500"
           >
-            Política de privacidade
+            <Link
+              as={RouterLink}
+              to="/"
+              maxW="170px"
+              textDecor="underline"
+              _hover={{ color: 'gray.400' }}
+            >
+              Política de privacidade
+            </Link>
+            <Link
+              as={RouterLink}
+              to="/"
+              maxW="120px"
+              textDecor="underline"
+              _hover={{ color: 'gray.400' }}
+            >
+              Termos de uso
+            </Link>
+            <Text>© {new Date().getFullYear()} AppJusto. Marca Registrada.</Text>
+          </Stack>
+          <Link as={RouterLink} to="/logout" mt={{ base: '4', md: '0'}} color="white" fontSize="sm" textDecor="underline" _hover={{ color: 'gray.400' }}>
+            {t('Sair')}
           </Link>
-          <Link
-            as={RouterLink}
-            to="/"
-            maxW="120px"
-            textDecor="underline"
-            _hover={{ color: 'gray.400' }}
-          >
-            Termos de uso
-          </Link>
-          <Text>© {new Date().getFullYear()} AppJusto. Marca Registrada.</Text>
-        </Stack>
+        </Flex>
       </Container>
     </Box>
   );
