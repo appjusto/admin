1e5f3f6942f55d70c2d6ca877ae110f9c845ef66
Renan Costa <renancostam@gmail.com>
2023-08-07 11:03:44 -0300

    Submitted scroll adjustment

--- src/pages/home/registration/Submitted.tsx
+++ src/pages/home/registration/Submitted.tsx
@@ -1,10 +1,14 @@
 import { Box, HStack, Icon, Text } from '@chakra-ui/react';
+import React from 'react';
 import { BsCheckCircle } from 'react-icons/bs';
 import { t } from 'utils/i18n';
 import { Collective } from '../dashboard/communication/Collective';
 import { MainVideo } from '../dashboard/communication/MainVideo';
 
 export const Submitted = () => {
+  React.useEffect(() => {
+    window?.scrollTo(0, 0);
+  }, []);
   return (
     <Box mt="6">
       <Box border="1px solid #B8E994" borderRadius="lg" bgColor="#F2FFE8" p="4">
